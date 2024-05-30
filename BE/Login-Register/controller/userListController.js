97d2a50f62be7d3f98fd85e44db1d2c792e38301
edrickh <107345629+edrickh@users.noreply.github.com>
2024-05-30 09:47:01 +0700

    Userlist

--- BE/Login-Register/controller/userListController.js
+++ BE/Login-Register/controller/userListController.js
@@ -1,6 +1,7 @@
 const config = require('../library/database');
 let mysql = require('mysql');
 let pool = mysql.createPool(config);
+const moment = require('moment');
 
 pool.on('error', (err) => {
     console.error(err);
@@ -12,13 +13,16 @@ module.exports = {
             pool.getConnection(function (err, connection) {
                 if (err) throw err;
                 connection.query(
-                    `SELECT * FROM user`, // Query all users
+                    `SELECT * FROM user`,
                     function (error, results) {
                         if (error) throw error;
+                        results.forEach(user => {
+                            user.last_login = moment(user.last_login).fromNow();
+                        });
                         res.render("userList", {
                             url: 'http://localhost:3000/',
                             nama_depan: req.session.nama_depan,
-                            users: results // Pass all users to the template
+                            users: results
                         });
                     }
                 );
@@ -28,5 +32,76 @@ module.exports = {
             console.error('Terjadi kesalahan:', error);
             res.status(500).send('Terjadi kesalahan pada server');
         }
+    },
+    getUserById(req, res) {
+        const userId = req.params.id;
+        try {
+            pool.getConnection(function (err, connection) {
+                if (err) throw err;
+                connection.query(
+                    `SELECT * FROM user WHERE user_id = ?`, [userId],
+                    function (error, results) {
+                        if (error) throw error;
+                        if (results.length > 0) {
+                            results[0].last_login = moment(results[0].last_login).fromNow(); // Format last_login
+                            res.render("userupdate", {
+                                url: 'http://localhost:3000/',
+                                nama_depan: req.session.nama_depan,
+                                user: results[0]
+                            
+                            });
+                        } else {
+                            res.status(404).send('User tidak ditemukan');
+                        }
+                    }
+                );
+                connection.release();
+            });
+        } catch (error) {
+            console.error('Terjadi kesalahan:', error);
+            res.status(500).send('Terjadi kesalahan pada server');
+        }
+    },
+    updateUser(req, res) {
+        const userId = req.params.id;
+        const { nama_depan, nama_belakang, email, no_tlpn, role } = req.body;
+        try {
+            pool.getConnection(function (err, connection) {
+                if (err) throw err;
+                connection.query(
+                    `UPDATE user SET nama_depan = ?, nama_belakang = ?, email = ?, no_tlpn = ?, role = ?, update_time = NOW() WHERE user_id = ?`,
+                    [nama_depan, nama_belakang, email, no_tlpn, role, userId],
+                    function (error, results) {
+                        if (error) throw error;
+                        res.redirect('/userlist');
+                    }
+                );
+                connection.release();
+            });
+        } catch (error) {
+            console.error('Terjadi kesalahan:', error);
+            res.status(500).send('Terjadi kesalahan pada server');
+        }
+    },
+    deleteUser(req, res) {
+        const userId = req.params.id;
+        try {
+            pool.getConnection(function (err, connection) {
+                if (err) throw err;
+                connection.query(
+                    `DELETE FROM user WHERE user_id = ?`,
+                    [userId],
+                    function (error, results) {
+                        if (error) throw error;
+                        res.redirect('/userlist');
+                    }
+                );
+                connection.release();
+            });
+        } catch (error) {
+            console.error('Terjadi kesalahan:', error);
+            res.status(500).send('Terjadi kesalahan pada server');
+        }
     }
+    
 };
--- BE/Login-Register/routes/userList.js
+++ BE/Login-Register/routes/userList.js
@@ -1,9 +1,10 @@
 var express = require('express');
 var router = express.Router();
-var { userList } = require('../controller/userListController');
-// var { verifyToken } = require('../controller/loginController');
+var { userList, getUserById, updateUser, deleteUser } = require('../controller/userListController');
 
-
-router.get('/', userList);  // Correct the route path to '/'
+router.get('/', userList);
+router.get('/update/:id', getUserById);
+router.post('/update/:id', updateUser);
+router.get('/delete/:id', deleteUser); // Add this line
 
 module.exports = router;
