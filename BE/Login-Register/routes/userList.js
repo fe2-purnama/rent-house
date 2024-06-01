092541292c02d6a22987ab4c50448b5699cbe8b6
edrickh <107345629+edrickh@users.noreply.github.com>
2024-06-01 11:42:39 +0700

    jwt edit

--- BE/Login-Register/routes/userList.js
+++ BE/Login-Register/routes/userList.js
@@ -1,10 +1,12 @@
 var express = require('express');
 var router = express.Router();
 var { userList, getUserById, updateUser, deleteUser } = require('../controller/userListController');
+// const { verifyToken } = require('../controller/loginController'); // Import the verifyToken middleware
 
-router.get('/', userList);
-router.get('/update/:id', getUserById);
-router.post('/update/:id', updateUser);
-router.get('/delete/:id', deleteUser); // Add this line
+// Protect the routes with verifyToken middleware
+router.get('/', /*verifyToken,*/ userList);
+router.get('/update/:id',/*verifyToken,*/ getUserById);
+router.post('/update/:id', /*verifyToken,*/ updateUser);
+router.get('/delete/:id', /*verifyToken,*/ deleteUser);
 
 module.exports = router;
