6895f9f754af85e05f110f616b4ec4a7477b34c0
edrickh <107345629+edrickh@users.noreply.github.com>
2024-06-17 21:59:17 +0700

    change

--- backend/library/database.js
+++ backend/library/database.js
@@ -1,7 +1,8 @@
 module.exports = {
     multipleStatements  : true,
-    host                : 'localhost',
-    user                : 'root',
-    password            : '',
-    database            : 'renthouse'
+    host                : 'educalab.id',
+    user                : 'eANIsB45Cj2atuuc',
+    password            : 'wXPTz5aSIlSxOYCI',
+    database            : 'MR3KGTTjGHtrpA8I',
+    port                : 3307
 };
